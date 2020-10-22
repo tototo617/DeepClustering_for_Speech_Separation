9eeb10c0affc28722e5694c636d438ac557da4d9
h_munakata <h_munakata@s3.kasukabe>
2020-10-22 20:41:43 +0900

    fix dir bug

--- create_env.sh
+++ create_env.sh
@@ -4,4 +4,5 @@ conda install pytorch torchvision cudatoolkit=10.2 -c pytorch # v1.6.0
 conda install -c conda-forge librosa # 0.8.0
 conda install -c anaconda pyyaml # 5.3.1
 conda install -c conda-forge tqdm # 4.50.2
+conda install -c conda-forge tensorboard # 2.3.0
 conda install -c conda-forge tensorboardx # 2.1
