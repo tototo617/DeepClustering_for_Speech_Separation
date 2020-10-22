17b3e2a86e308a39aec5d68154889482f054ba3e
h_munakata <h_munakata@s3.kasukabe>
2020-10-22 20:23:49 +0900

    test

---
+++ create_env.sh
@@ -0,0 +1,7 @@
+conda create -n DC python=3.8.3
+conda activate DC
+conda install pytorch torchvision cudatoolkit=10.2 -c pytorch # v1.6.0
+conda install -c conda-forge librosa # 0.8.0
+conda install -c anaconda pyyaml # 5.3.1
+conda install -c conda-forge tqdm # 4.50.2
+conda install -c conda-forge tensorboardx # 2.1
